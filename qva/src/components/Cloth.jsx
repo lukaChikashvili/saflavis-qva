21f8be767c852005e55a54239e1c6bddead1b2c8
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-06 19:52:51 +0400

    add cloth wave animation

--- qva/src/components/Cloth.jsx
+++ qva/src/components/Cloth.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import vertex from '../shaders/clothShader/vertex.glsl';
 import fragment from '../shaders/clothShader/fragment.glsl';
+import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber'
 
 const Cloth = () => {
+
+  // uniforms
+  const uniforms = useRef({
+    uTime: { value: 0},
+    uHeight: { value: 0.2 },
+    uFrequency: { value: new THREE.Vector2(4, 1.5)}
+  });
+
+
+  // animate cloth
+    useFrame(() => {
+      uniforms.current.uTime.value += 0.004;
+    })
   return (
    <>
      <mesh>
@@ -11,6 +26,7 @@ const Cloth = () => {
           wireframe = {true}
           vertexShader={vertex}
           fragmentShader={fragment}
+          uniforms = {uniforms.current}
         />
      </mesh>
    </>
--- qva/src/shaders/clothShader/vertex.glsl
+++ qva/src/shaders/clothShader/vertex.glsl
@@ -1,5 +1,14 @@
+uniform float uHeight;
+uniform vec2 uFrequency;
+uniform float uTime;
+
 void main() {
-       vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+   float elevation = sin(modelPosition.x * uFrequency.x + uTime) * 
+                     sin(modelPosition.z * uFrequency.y + uTime) * uHeight;
+   modelPosition.y += elevation;
+
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
